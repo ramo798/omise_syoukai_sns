b6ef0a378aae0c077b5f8695b9be4751999c8759
ramo798 <knht31i@gmail.com>
2020-08-18 20:20:44 +0900

    SQL初期データの投入

---
+++ docker/db/sql/001-create-tables.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee
+(
+  id           INT(10),
+  name     VARCHAR(40)
+);
+
+INSERT INTO employee (id, name) VALUES (1, "Nagaoka");
+INSERT INTO employee (id, name) VALUES (2, "Tanaka");
