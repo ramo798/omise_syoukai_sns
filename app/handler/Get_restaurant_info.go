bc970ab3b476a057c1f78d0f9092c5cd6aa6027b
ramo798 <knht31i@gmail.com>
2020-08-19 00:27:11 +0900

    存在しないIDを渡されたときのエラー処理を書かないといけない

--- app/handler/Get_restaurant_info.go
+++ app/handler/Get_restaurant_info.go
@@ -1,8 +1,6 @@
 package handler
 
 import (
-	"fmt"
-
 	"github.com/gin-gonic/gin"
 	"github.com/ramo798/omise_syoukai_sns/db"
 	"github.com/ramo798/omise_syoukai_sns/model"
@@ -12,9 +10,19 @@ func Get_restaurant_info(c *gin.Context) {
 
 	var target_id model.Receiver_restaurant_info
 	c.BindJSON(&target_id)
+	// fmt.Println(target_id)
 	a := db.Res_restaurant_info(target_id.Restaurant_id)
-	fmt.Println(a)
+	// fmt.Println(a[0])
 
-	// res := "eee"
-	// c.String(200, res)
+	tmp := a[0]
+
+	// jsonBytes, err := json.Marshal(tmp)
+	// if err != nil {
+	// 	fmt.Print(err)
+	// 	return
+	// }
+	// res := string(jsonBytes)
+	// fmt.Println(res)
+
+	c.JSON(200, tmp)
 }
