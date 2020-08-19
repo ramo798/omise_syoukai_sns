9661d751539b975ff4f076b1fbc885960a954603
ramo798 <knht31i@gmail.com>
2020-08-19 21:59:23 +0900

    MAKE new SQL table post_list

--- docker/db/sql/001-create-tables.sql
+++ docker/db/sql/001-create-tables.sql
@@ -25,3 +25,19 @@ CREATE TABLE restaurant_info
 INSERT INTO restaurant_info (id, name,address,logo_image_url,station_name) VALUES ("J0000", "鳥貴族","pa-kus2121u","htttp/11111","なんば");
 INSERT INTO restaurant_info (id, name,address,logo_image_url,station_name) VALUES ("J01200", "鳥貴族1212","pa-k1212usu","htttp/1111121","なん1ば");
 INSERT INTO restaurant_info (id, name,address,logo_image_url,station_name) VALUES ("J0002220", "鳥貴族212","pa-k1212usu","htttp/12121111","な1111んば");
+
+
+DROP TABLE IF EXISTS post_list;
+
+CREATE TABLE post_list
+(
+  id INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
+  users_id VARCHAR(30),
+  comment VARCHAR(240),
+  restaurant_id VARCHAR(100),
+  price INT,
+  assessment INT
+);
+INSERT INTO post_list (users_id,comment,restaurant_id,price,assessment) VALUES ("ramo123", "めちゃおいしいです。","J0000",4500,80);
+INSERT INTO post_list (users_id,comment,restaurant_id,price,assessment) VALUES ("ramo456", "めちゃおいしいです。sasasa","J0000",4500,80);
+INSERT INTO post_list (users_id,comment,restaurant_id,price,assessment) VALUES ("ramo789", "めちゃおいしいです。sasasa","J0000",4500,80);
