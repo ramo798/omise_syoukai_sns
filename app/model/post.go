817c6b1aadb84c0519a7cd05128606338c6c233d
ramo798 <knht31i@gmail.com>
2020-08-22 17:43:07 +0900

    ADD　記事投稿用のmodelの追加

--- app/model/post.go
+++ app/model/post.go
@@ -1,8 +1,17 @@
 package model
 
-// Post is 投稿用の構造体
+// Post is ポストの構造体
 type Post struct {
 	ID           int    `json:"post_id"`
+	UsersID      string `json:"users_id"`
+	Comment      string `json:"comment"`
+	RestaurantID string `json:"sestaurant_id"`
+	Price        int    `json:"price"`
+	Assessment   int    `json:"assessment"`
+}
+
+// PostForRegister is ポストの投稿用の構造体
+type PostForRegister struct {
 	UsersID      string `json:"users_id"`
 	Comment      string `json:"comment"`
 	RestaurantID string `json:"sestaurant_id"`
