bfaeecc8c5a565c8f96bcc14e696ac3b1895864f
ramo798 <knht31i@gmail.com>
2020-08-22 16:40:45 +0900

    ADD 特定ユーザの投稿取得のハンドラーの実装

--- app/handler/postlist.go
+++ app/handler/postlist.go
@@ -18,3 +18,17 @@ func GetAllPost(c *gin.Context) {
 	}
 	c.JSON(200, posts)
 }
+
+// GetUserPost is 特定のユーザのポスト一覧をjsonで返す。
+func GetUserPost(c *gin.Context) {
+	// http://~~~.com/post/xxx のxxxをストリングでとる処理
+	username := c.Param("name")
+	posts, err := db.GetterUserPost(username)
+	if err != nil {
+		log.Println("err")
+	}
+	for _, j := range posts {
+		log.Printf("%#v", j)
+	}
+	c.JSON(200, posts)
+}
